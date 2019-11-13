3d689f53f15f9ce0afebe4ba17bb3c59f66e62e2
guilhem hermet <hermetguilhem@gmail.com>
2019-11-13 10:54:06 +0100

    ajout des services et debut d'implementation de la classe TraiterRecherche

---
+++ Spotmimsfy/src/main/java/general/SPARQLService.java
@@ -0,0 +1,15 @@
+package general;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author herme
+ */
+public class SPARQLService {
+    
+}
